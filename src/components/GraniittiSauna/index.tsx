17a3ca8c6247b7396520dd0b765ae40a49628050
Laged <parkkila.matti@gmail.com>
2025-10-14 20:29:35 +0300

    chore: graniittimatkat copy

--- src/components/GraniittiSauna/index.tsx
+++ src/components/GraniittiSauna/index.tsx
@@ -35,16 +35,18 @@ export default function GraniittiSauna({
             <div className={styles.description}>
               <p>
                 Kaipaatko silmiä siristäviä löylyjä graniittisaunassa,
-                kutkuttavia mysteereitä ja lupaa tehdä katalia petoksia? Olitpa
-                sitten kokenut, mutta epäonnekas seikkailija, jolle Faaraon
-                kirous jäi viime kerralla hampaankoloon tai innokas tulokas,
-                joka janoaa Kaken legendaarista tomaatti-sellerisoppaa – meillä
-                on tarjota sinulle jotakin aivan uskomatonta.
+                kutkuttavia mysteereitä ja vapaat kädet tehdä jotakin todella,
+                siis TODELLA, peruuttamatonta? Olitpa sitten vanha ja
+                päättäväinen tekijä, joka yhä yrittää päästää Faaraon kirouksen
+                valloilleen, tai uusi ja vähän liian innokas sielu, joka janoaa
+                Kaken ylistettyä tomaatti-sellerisoppaa – me lupaamme sinulle
+                jotain aivan uskomatonta. Uskomattoman hyvää…tai pahaa… sen
+                päätät sinä.
               </p>
 
               <p>
-                Graniittimatkat suuntaavat jälleen tuttuihin maisemiin,
-                idylliseen{" "}
+                Suuntaamme jälleen sinne, missä itse pahuus juurtaa juurensa:
+                idylliseen ja viattomalta vaikuttavaan{" "}
                 <a
                   href="https://www.storfinnhova.com/"
                   target="_blank"
@@ -53,13 +55,15 @@ export default function GraniittiSauna({
                 >
                   Storfinnhova Gårdiin
                 </a>
-                , ensi vuoden toukokuussa. Yleisön pyynnöstä aloitamme
-                murharituaalit jo torstaina ja päätämme ne sunnuntaina.
+                . Yleisön painostuksesta (ja omien kierojen mielihalujemme
+                takia) aloitamme rituaalit jo torstaina ja sunnuntaina aamun
+                sarastaessa laskemme rivejä josko joku on vielä hengissä.
               </p>
 
               <p>
-                Hyppää siis mukaan tähän ainutlaatuiseen matkaan täyttämällä
-                ilmoittautumislomake alapuolelta. Nähdään Storfinnhovassa!
+                Alkoiko pelottamaan vai mitä vielä odotat..? Tartu tähän
+                tilaisuuteen ja hyppää mukaan täyttämällä ilmoittautumislomake
+                alapuolelta. Nähdään Storfinnhovassa!
               </p>
 
               <p className={styles.highlight}>
